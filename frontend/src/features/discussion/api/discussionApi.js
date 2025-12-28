6ac1f379feaff1c456bc12d8a152e2b23681f79a
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2025-12-28 20:16:22 +0545

    Resolved merge conflicts on bugs branch

--- frontend/src/features/discussion/api/discussionApi.js
+++ frontend/src/features/discussion/api/discussionApi.js
@@ -6,11 +6,11 @@ export const getDiscussionById = (id) => api.get(`/discussion/${id}`);
 export const createDiscussion = (data) => api.post("/discussion", data);
 export const updateDiscussion = (id, data) => api.put(`/discussion/${id}`, data);
 export const deleteDiscussion = (id) => api.delete(`/discussion/${id}`);
-export const voteDiscussion = (id, voteType) => api.post(`/discussion/${id}/vote`, { voteType });
+export const voteDiscussion = (id) => api.post(`/discussion/${id}/vote`, { voteType: 'upvote' });
 
 export const updateComment = (id, commentId, content) => api.put(`/discussion/${id}/comments/${commentId}`, { content });
 export const deleteComment = (id, commentId) => api.delete(`/discussion/${id}/comments/${commentId}`);
-export const voteComment = (id, commentId, voteType) => api.post(`/discussion/${id}/comments/${commentId}/vote`, { voteType });
+export const voteComment = (id, commentId) => api.post(`/discussion/${id}/comments/${commentId}/vote`, { voteType: 'upvote' });
 export const pinDiscussion = (id) => api.post(`/discussion/${id}/pin`);
  
 
